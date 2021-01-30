73490955ada75d84432c555121a4d9d1f65778fc
Blake Ruwali <75585127+blakeruwali@users.noreply.github.com>
2021-01-30 09:45:53 -0500

    Added Line Chart

--- HeadacheOverallbyAgeGroupLineChart.R
+++ HeadacheOverallbyAgeGroupLineChart.R
@@ -16,20 +16,34 @@ migraine <- migraine %>% mutate(agegroup = case_when(age <= 25 ~ 'Below 25',
                                                      age >= 25  & age <= 40 ~ '25 - 40',
                                                      age >= 40  & age <= 55 ~ '40 - 55',
                                                      age >= 55  & age <= 70 ~ '55 - 70'
-                                             )) # end function
+)) # end function
 
 head(migraine)
 
-migrainecount <- migraine %>%  
-  group_by(agegroup, headache) %>% 
+migraine_headache = filter(migraine, headache == "yes")
+
+head(migraine_headache)
+
+migrainecount <- migraine_headache %>%  
+  group_by(agegroup, sex) %>% 
   summarise(Freq = n()) 
 migrainecount  
 
-migrainecount$agegroup <- factor(migrainecount$agegroup, levels = c('Below 25', '25 - 40', '40 - 55','55 - 70'))
-
-agegroupviz <- ggplot(migrainecount, aes(x=agegroup, y=Freq, fill=headache)) + 
-  geom_bar(stat="identity", position="dodge")
+agegroupviz <- ggplot(migrainecount, aes(x=agegroup, y=Freq, group=sex)) + 
+  geom_line(aes(color=sex))+
+  geom_point(aes(color=sex))
 
 agegroupviz <- agegroupviz + ggtitle("Headache by Age Group") + xlab("Age Group") + ylab("Headache Cases") # Adds titles
 
 agegroupviz
+
+migrainecount$agegroup <- factor(migrainecount$agegroup, levels = c('Below 25', '25 - 40', '40 - 55','55 - 70'))
+
+agegroupviz <- ggplot(migrainecount, aes(x=agegroup, y=Freq, group=sex)) + 
+  geom_line(aes(color=sex))+
+  geom_point(aes(color=sex))
+
+agegroupviz <- agegroupviz + ggtitle("Headache by Age Group") + xlab("Age Group") + ylab("Headache Cases") # Adds titles
+
+agegroupviz
+
