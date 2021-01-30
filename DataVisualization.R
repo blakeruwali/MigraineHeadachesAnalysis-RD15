2d8df02a01e389dbed8f8cf0f162882c2c140551
Blake Ruwali <75585127+blakeruwali@users.noreply.github.com>
2021-01-29 23:51:59 -0500

    Update DataVisualization.R
    Added new visualization (PieChart)

--- DataVisualization.R
+++ DataVisualization.R
@@ -8,3 +8,41 @@ migraine <- read.csv(file = 'G15dataset_KosteckiDillon.csv')
 # Printing the data
 head(migraine)
 
+# Use reshape2
+library(dplyr)
+
+viz1_pre <- migraine %>%
+  count(headache, sex, sort = TRUE) 
+
+head(viz1_pre)
+
+library(tidyr)
+
+viz1_final <-viz1_pre %>% spread(sex,n)
+head(viz1_final)
+
+bp<- ggplot(viz1_final, aes(x="", y=female, fill=headache))+
+  geom_bar(width = 1, stat = "identity")
+pie <- bp + coord_polar("y", start=0)
+pie
+
+bp2<- ggplot(viz1_final, aes(x="", y=male, fill=headache))+
+  geom_bar(width = 1, stat = "identity")
+pie2 <- bp2 + coord_polar("y", start=0)
+pie2
+
+p = ggplot(viz1_pre, aes(x = factor(1), y = n, fill = factor(headache))) + 
+  geom_bar(stat = "identity", width = 1) + 
+  theme(legend.position = "none") +
+  scale_x_discrete(NULL, expand = c(0,0)) +
+  scale_y_continuous(NULL, expand = c(0,0)) + 
+  coord_polar(theta = "y") +
+  facet_wrap(~sex)
+p
+p <- p + ggtitle("Headache by Sex for Migraine Patients") + xlab("") + ylab("Sex") # Adds titles
+
+p <- p + facet_grid(facets=. ~ sex) # Side by side bar chart
+
+p <- p + coord_polar("y") # side by side pie chart
+p
+
